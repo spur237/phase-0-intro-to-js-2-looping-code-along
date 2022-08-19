283d2d1a410f56844f5c400c979ae8fdf74d11bb
spur237 <samuel.nzioka@student.moringaschool.com>
2022-08-19 19:06:42 +0300

    compelete assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+const countDown = (number) => {
+    let no = number;
+    while (no >= 0){
+        console.log(no--)
+    }
+}
+
+function writeCards(names, content){
+    const messages = []
+
+    for(let x = 0; x < names.length; x++){
+        const message = `Thank you, ${names[x]}, for the wonderful ${content} gift!`
+        messages.push(message)
+    }
+
+    // names.forEach((name) => {
+    //     let message = `Thank you, ${names}, for the wonderful ${content} gift!`
+    //     messages.push(message)
+    // });
+
+    return messages;
+}
+
+console.log(writeCards(['Chalrs', 'Scah'], "crazy")) 
